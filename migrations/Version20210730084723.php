f51adefa3d9b4b5fa875129ce1cf789841bdab06
alexis <alexis.reaupro@gmail.com>
2021-07-30 11:02:13 +0200

    mise à jour image produit

--- migrations/Version20210730084723.php
+++ migrations/Version20210730084723.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210707130446 extends AbstractMigration
+final class Version20210730084723 extends AbstractMigration
 {
     public function getDescription(): string
     {
